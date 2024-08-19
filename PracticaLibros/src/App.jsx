2796fc0423a9c0b61e3690f0dafac487ab6c9859
Cristian Ninotti <cristianninotti03@gmail.com>
2024-08-19 17:50:02 -0300

    Practica Libros Terminada

--- PracticaLibros/src/App.jsx
+++ PracticaLibros/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import BookItem from './componentes/bookItem/BookItem'
+import books from './componentes/books/books'
+
 
 function App() {
 
   return (
     <>
-      
+      <BookItem books = {books}/>
     </>
   )
 }
