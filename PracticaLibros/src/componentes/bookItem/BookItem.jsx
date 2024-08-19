c0efdc8836001d9ee8f2d4dda13c5f4e6c77b356
Cristian Ninotti <cristianninotti03@gmail.com>
2024-08-19 17:51:21 -0300

    Espaciado para legibilidad

--- PracticaLibros/src/componentes/bookItem/BookItem.jsx
+++ PracticaLibros/src/componentes/bookItem/BookItem.jsx
@@ -8,19 +8,22 @@ const BookItem = ({ books }) => {
             <div className="d-flex justify-content-center flex-wrap">
 
                 {books.map(({ Id, Title, Author, Rating, pageCount, imageUrl }) =>
+
                     <Card key={Id} className="mx-3" style={{ width: "22rem" }}>
+
                         <Card.Body>
+                            
                             <Card.Title>{Title}</Card.Title>
                             <Card.Subtitle>{Author}</Card.Subtitle>
                             <div>{Rating.length} estrellas</div>
                             <p>{pageCount} páginas</p>
                             <Card.Img height={400} variant="top" src={imageUrl !== "" ? imageUrl : "https://bit.ly/47NylZk"}/>
                             <hr></hr>
+
                         </Card.Body>
+
                     </Card>
                 )}
-
-
 
             </div>
         </>
